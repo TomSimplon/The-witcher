fafcc777dbb60416e0c96af6249740e218135466
tom <tom.pustel.simplon@gmail.com>
2023-11-19 19:19:57 +0100

    pagination de la page des articles dynamique

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,44 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const articles = document.querySelectorAll('.article');
+  const totalPages = Math.ceil(articles.length / 4);
+  let currentPage = 1;
+
+  const prevButton = document.querySelector('.previous_page');
+  const nextButton = document.querySelector('.next_page');
+
+  function updateButtonVisibility() {
+      prevButton.style.display = (currentPage > 1) ? '' : 'none';
+      nextButton.style.display = (currentPage < totalPages) ? '' : 'none';
+  }
+
+  function showPage(page) {
+      const start = (page - 1) * 4;
+      const end = start + 4;
+      articles.forEach((article, index) => {
+          article.style.display = (index >= start && index < end) ? '' : 'none';
+      });
+      document.querySelector('.page_number').textContent = 'Page ' + page;
+
+      updateButtonVisibility();
+  }
+
+  prevButton.addEventListener('click', function () {
+      if (currentPage > 1) {
+          currentPage--;
+          showPage(currentPage);
+      }
+  });
+
+  nextButton.addEventListener('click', function () {
+      if (currentPage < totalPages) {
+          currentPage++;
+          showPage(currentPage);
+      }
+  });
+
+  showPage(1);
+});
